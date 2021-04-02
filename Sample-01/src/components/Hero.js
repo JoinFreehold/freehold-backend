110f4dea214cdd90d5804befe3162f6a8ba79717
Jason Schrader <whoabuddy@users.noreply.github.com>
2021-04-02 16:22:02 -0700

    add some freehold flair

--- Sample-01/src/components/Hero.js
+++ Sample-01/src/components/Hero.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-import logo from "../assets/logo.svg";
+import logo from "../assets/logo-icon.png";
 
 const Hero = () => (
   <div className="text-center hero my-5">
-    <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
-    <h1 className="mb-4">React.js Sample Project</h1>
+    <img className="mb-3 app-logo" src={logo} alt="Freehold logo" width="120" />
+    <h1 className="mb-4">Freehold Sample Project</h1>
 
     <p className="lead">
       This is a sample application that demonstrates an authentication flow for
-      an SPA, using <a href="https://reactjs.org">React.js</a>
+      an SPA, using <a href="https://reactjs.org">React.js</a> and <a href="https://auth0.com">Auth0</a>
     </p>
   </div>
 );
--- Sample-01/src/utils/contentData.js
+++ Sample-01/src/utils/contentData.js
@@ -1,27 +1,27 @@
 const contentData = [
   {
-    title: "Configure other identity providers",
+    title: "Provide a simple experience",
     link: "https://auth0.com/docs/connections",
     description:
-      "Auth0 supports social providers as Facebook, Twitter, Instagram and 100+, Enterprise providers as Microsoft Office 365, Google Apps, Azure, and more. You can also use any OAuth2 Authorization Server."
+      "When first joining Freehold, an email code is sent and BOOM you are in!"
   },
   {
-    title: "Enable Multifactor Authentication",
+    title: "Link to Active Campaign",
     link: "https://auth0.com/docs/multifactor-authentication",
     description:
-      "Add an extra layer of security by enabling Multi-factor Authentication, requiring your users to provide more than one piece of identifying information. Push notifications, authenticator apps, SMS, and DUO Security are supported."
+      "Since this is our main CRM, we can leverage it and keep the data where it should be."
   },
   {
-    title: "Anomaly Detection",
+    title: "Implement cool features",
     link: "https://auth0.com/docs/anomaly-detection",
     description:
       "Auth0 can detect anomalies and stop malicious attempts to access your application. Anomaly detection can alert you and your users of suspicious activity, as well as block further login attempts."
   },
   {
-    title: "Learn About Rules",
+    title: "Initiate Global Takeover",
     link: "https://auth0.com/docs/rules",
     description:
-      "Rules are JavaScript functions that execute when a user authenticates to your application. They run once the authentication process is complete, and you can use them to customize and extend Auth0's capabilities."
+      "The same thing we do every night, Pinky - try to take over the world!"
   }
 ];
 
