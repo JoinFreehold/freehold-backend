a205274878087eb1250a8a1ad91589e30033dab4
Jason Schrader <whoabuddy@users.noreply.github.com>
2021-04-05 06:34:40 -0700

    add eslint plugin react hooks

--- Sample-01/src/components/Content.js
+++ Sample-01/src/components/Content.js
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
-
 import { Row, Col } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 import contentData from "../utils/contentData";
 
 class Content extends Component {
 
   render() {
-    
+
     return (
       <div className="next-steps my-5">
         <h2 className="my-5 text-center">What can I do next?</h2>
@@ -26,7 +24,7 @@ class Content extends Component {
           ))}
         </Row>
       </div>
-    );
+      );
   }
 }
 
